d1de8c47d6a51135ab020352a81974ccc4b50d9f
Mohamed-A-Badr <badr49960@gmail.com>
2024-12-22 18:00:50 +0200

    Login Function

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 
-def register(request):
+def register_view(request):
     form = RegisterForm()
     if request.method == "POST":
         form = RegisterForm(request.POST)
@@ -23,5 +23,28 @@ def register(request):
         "accounts/register.html",
         {
             "form": form,
+        },
+    )
+
+
+def login_view(request):
+    error_message = None
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            next_url = (
+                request.POST.get("next") or request.GET.get("next") or "books:book_list"
+            )
+            return redirect(next_url)
+        else:
+            error_message = "Invalid credentials."
+    return render(
+        request,
+        "accounts/login.html",
+        {
+            "error": error_message,
         },
     )
--- bookstore/settings.py
+++ bookstore/settings.py
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+LOGIN_REDIRECT_URL = "books:book_list"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
